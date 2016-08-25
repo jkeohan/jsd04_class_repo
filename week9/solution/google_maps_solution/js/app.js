e551a641a52e2ceb1fe5c2d0eddc66bee6f64b72
Joe Keohah <jkeohan@gmail.com>
2016-08-25 18:26:37 -0400

    “updates”

--- week9/solution/google_maps_solution/js/app.js
+++ week9/solution/google_maps_solution/js/app.js
@@ -27,6 +27,7 @@ $(document).ready(function() {
 
 	// Build bike markers from the CitiBike data
 	function buildBikeMarkers (stations) {
+		var infowindow = new google.maps.InfoWindow()
 		// loop overs stations array and use coordinates to append each station to myMap
 		stations.forEach(function (station) {
 			// we have to divide each value by 1000000 due to the data format
@@ -36,18 +37,19 @@ $(document).ready(function() {
 		    var marker = new google.maps.Marker({
 		        position: {lat: lat, lng: lng},
 		        map: myMap,
-	            title: station.name,
+	          title: station.name,
 		    });
 
 			// create InfoWindow HTML
-            var content = '<div>' + station.name + '<br>' + station.bikes +' bikes, ' + station.free + ' racks free.</div>'
+        var content = '<div>' + station.name + '<br>' + station.bikes +' bikes, ' + station.free + ' racks free.</div>'
 
-        	var infowindow = new google.maps.InfoWindow({
-            	content: content,
-        	});
-
+        	// var infowindow = new google.maps.InfoWindow({
+         //    	content: content,
+        	// });
     		// add an event listener to your marker to open the InfoWindow on click
         	marker.addListener('click', function() {
+        			infowindow.close()
+        			infowindow.setContent(content)
             	infowindow.open(map, marker);
         	});
 		})
---
+++ week9/solution/node_maps_solution/README.md
@@ -0,0 +1,8 @@
+# Node Maps
+
+## Setup
+
+1. Run `npm install` to install npm modules
+2. To kick off server, run `nodemon` from the command line (then visit `http://localhost:1337`)
+
+Note: if `nodemon` fails, you might have to install it globally: `npm install -g nodemon`
